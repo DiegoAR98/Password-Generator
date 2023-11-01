60cdafe2a46e88a000f95c343534eec636554db1
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2023-11-01 18:38:09 -0400

    Added working code to the project

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,45 @@
 // Assignment code here
-// Character sets
+
+// Convert string into an array
 var nums = "0123456789".split("");
 var special = "!%&,.*+-./<>?~".split("");
 var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
-var wins = 0;
-var ties = 0;
-var losses = 0;
+// Generate Password function
+function generatePassword() {
+  var passwordLength = prompt("Choose password length (8-50):");
 
-// Array of options for computer to pick from
-var options = ["R", "P", "S"];
+  while (passwordLength < 8 || passwordLength > 50) {
+    alert("Password length must be between 8-50 characters. Try again.");
+    passwordLength = prompt("Choose password length (8-50):");
+  }
 
-var playGame = function () {
-  // example prompt call
-  var userChoice = window.prompt("Enter R, P, or S:");
-  window.alert("User Choice is " + userChoice);
+  var includeSpecialCharacters = confirm("Click OK to include special characters.");
+  var includeNumbers = confirm("Click OK to include numeric characters.");
+  var includeLowerCase = confirm("Click OK to include lowercase characters.");
+  var includeUpperCase = confirm("Click OK to include uppercase characters.");
 
-  // Ask user for their choice
+    // Check and ensure that at least one character type is chosen
+  while (!includeSpecialCharacters && !includeNumbers && !includeLowerCase && !includeUpperCase) {
+    alert("You must choose at least one character type.");
+    return generatePassword(); // Restart the function to let the user choose again.
+  }
 
-  // Get random index from array of options
+    // Concatenate character arrays based on user's choices
+  var possibleCharacters = [];
+  if (includeSpecialCharacters) possibleCharacters = possibleCharacters.concat(special);
+  if (includeNumbers) possibleCharacters = possibleCharacters.concat(nums);
+  if (includeLowerCase) possibleCharacters = possibleCharacters.concat(lowerCase);
+  if (includeUpperCase) possibleCharacters = possibleCharacters.concat(upperCase);
 
-  // If choices are the same, it's a tie
+  var password = '';
+  for (let i = 0; i < passwordLength; i++) {
+    password += possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)];
+  }
 
-  // Check every win condition for the player
-
-  // If above conditions failed, assume player lost
-
-  // Print stats
-
-  // Ask user to play again
-
-  // If user pressed OK, run the function again
-};
-
-// Run the game for the first time
-playGame();
-
-
-
+  return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -49,7 +50,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
